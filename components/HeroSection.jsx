42459a15ec230ca717696d2e7aa06d36ad4329c3
JoshuaToback <joshuatoback@gmail.com>
2023-10-05 21:22:52 -0400

    Added Resume link

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -29,7 +29,7 @@ const HeroSection = () => {
                 "Web Developer",
                 1000,
                 "Next.js Developer",
-                1000
+                1000,
               ]}
               wrapper="span"
               speed={50}
@@ -37,7 +37,8 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Joshua Toback is a front-end developer and UI/UX designer based in Los Angeles, CA.
+            Joshua Toback is a front-end developer and UI/UX designer based in
+            Los Angeles, CA.
           </p>
           <div>
             <Link
@@ -47,8 +48,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-                href="/public/resume.pdf"  
-                download="YourResume.pdf"       
+              href="https://drive.google.com/file/d/1H0t-tEsagWaszCAuThQVJRcqgcocXNHe/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-400 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
