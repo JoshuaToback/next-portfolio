3c645f9c0353d4e4923bc2067dd957957805fa8c
JoshuaToback <joshuatoback@gmail.com>
2023-10-05 21:15:31 -0400

    Removed apostrophes

--- components/EmailSection.jsx
+++ components/EmailSection.jsx
@@ -13,11 +13,11 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-3xl font-bold text-white my-2">
-          Let&apos;s Connect!
+          Let's Connect!
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I&apos;m looking to start my career as a Front End Developer.{" "} 
+          I'm looking to start my career as a Front End Developer.{" "} 
           If you'd like to collaborate together, feel free to reach out!
         </p>
         <div className="socials flex flex-row gap-2">
