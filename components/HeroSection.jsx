ec90ff628011ab2f7a95514830b0b8d63ee6c14c
JoshuaToback <joshuatoback@gmail.com>
2023-10-24 15:46:24 -0400

    Debugged Hero section buttons

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
-import Link from "next/link";
+
+import { Link } from "react-scroll";
 
 import Joshua from "@/images/headshot.png";
+import { ScrollLink } from "react-scroll";
 
 const HeroSection = () => {
   return (
@@ -41,19 +43,25 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              className="px-6 cursor-pointer inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Hire Me
             </Link>
-            <Link
+            <a
               href="https://drive.google.com/file/d/1H0t-tEsagWaszCAuThQVJRcqgcocXNHe/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer" // Important for security
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-400 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
-            </Link>
+            </a>
           </div>
         </motion.div>
         <motion.div
